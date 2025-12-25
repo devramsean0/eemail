4fe1280137b431efdf0a9a96fba403e1fd888216
Sean Outram <me@sean.cyou>
2025-12-25 20:58:28 +0000

    DATA PARSING (WHOOOOO)

--- components/smtp/src/message_handler.rs
+++ components/smtp/src/message_handler.rs
@@ -11,6 +11,8 @@ struct Mail {
     from: String,
     to: Vec<String>,
     data: String,
+
+    sending_data: bool,
 }
 
 pub async fn handle_smtp(stream: TcpStream, config: PortConfiguration) -> anyhow::Result<()> {
@@ -38,64 +40,110 @@ pub async fn handle_smtp(stream: TcpStream, config: PortConfiguration) -> anyhow
             .map(str::to_string)
             .collect();
 
-        if let Some(first) = cmd.get(0) {
-            if let Some(second) = cmd.get(1) {
-                debug!("Received Command: {} with data: {}", first, second);
+        if !mail.sending_data {
+            if let Some(first) = cmd.get(0) {
+                if let Some(second) = cmd.get(1) {
+                    debug!("Received Command: {} with data: {}", first, second);
+                } else {
+                    debug!("Received Command: {}", first)
+                }
+                match first.as_str() {
+                    "EHLO" => {
+                        let mut extension_strs: Vec<String> = vec![
+                            "250-Localhost".to_string(),
+                            "250-PIPELINING".to_string(),
+                            format!("250-Size {}", 10 * 1024 * 1024),
+                        ];
+
+                        if config.auth_enabled {
+                            extension_strs.push("250-AUTH PLAIN LOGIN".to_string());
+                        }
+
+                        // The last one in the array needs to be "250 " not "250-"
+                        if let Some(last) = extension_strs.last_mut() {
+                            *last = last.replacen("250-", "250 ", 1);
+                        }
+
+                        debug!("Sending EHLO Response {:#?}", extension_strs);
+                        let mut joined_extensions = extension_strs.join("\r\n");
+                        joined_extensions.push_str("\r\n");
+                        writer.write_all(joined_extensions.as_bytes()).await?;
+                    }
+                    "MAIL" => {
+                        if let Some(second) = cmd.get(1) {
+                            // Carefully strip out the FROM header, this caters to Thunderbirds specific behaviour so some leeway is allowed
+                            let second = second
+                                .strip_prefix("FROM:")
+                                .unwrap_or_else(|| second)
+                                .to_string();
+                            let second = second
+                                .strip_prefix("<")
+                                .unwrap_or_else(|| &second)
+                                .strip_suffix(">")
+                                .unwrap_or_else(|| &second)
+                                .to_string();
+
+                            debug!("Stripped FROM header to {}", second);
+
+                            writer.write_all(&message_formatter("250 OK")).await?;
+
+                            mail.from = second;
+                            debug!("Responded to MAIL FROM");
+                        }
+                    }
+                    "RCPT" => {
+                        if let Some(second) = cmd.get(1) {
+                            // Carefully strip out the FROM header, this caters to Thunderbirds specific behaviour so some leeway is allowed
+                            let second = second
+                                .strip_prefix("TO:")
+                                .unwrap_or_else(|| second)
+                                .to_string();
+                            let second = second
+                                .strip_prefix("<")
+                                .unwrap_or_else(|| &second)
+                                .strip_suffix(">")
+                                .unwrap_or_else(|| &second)
+                                .to_string();
+
+                            debug!("Stripped TO header to {}", second);
+                            mail.to.push(second);
+                            writer.write_all(&message_formatter("250 OK")).await?;
+                            debug!("Responded to RCPT TO");
+                        }
+                    }
+                    "DATA" => {
+                        // Validations
+                        if mail.from.is_empty() || mail.to.is_empty() {
+                            writer
+                                .write_all(&message_formatter("503 Bad Sequence of commands"))
+                                .await?;
+                        }
+                        mail.sending_data = true;
+                        writer
+                            .write_all(&message_formatter("354 End data with <CR><LF>.<CR><LF>"))
+                            .await?;
+                    }
+                    _ => {
+                        warn!("Unrecognised Command {}", first);
+                    }
+                }
             } else {
-                debug!("Received Command: {}", first)
-            }
-
-            match first.as_str() {
-                "EHLO" => {
-                    let mut extension_strs: Vec<String> = vec![
-                        "250-Localhost".to_string(),
-                        "250-PIPELINING".to_string(),
-                        format!("250-Size {}", 10 * 1024 * 1024),
-                    ];
-
-                    if config.auth_enabled {
-                        extension_strs.push("250-AUTH PLAIN LOGIN".to_string());
-                    }
-
-                    // The last one in the array needs to be "250 " not "250-"
-                    if let Some(last) = extension_strs.last_mut() {
-                        *last = last.replacen("250-", "250 ", 1);
-                    }
-
-                    debug!("Sending EHLO Response {:#?}", extension_strs);
-                    let mut joined_extensions = extension_strs.join("\r\n");
-                    joined_extensions.push_str("\r\n");
-                    writer.write_all(joined_extensions.as_bytes()).await?;
-                }
-                "MAIL" => {
-                    if let Some(second) = cmd.get(1) {
-                        // Carefully strip out the FROM header, this caters to Thunderbirds specific behaviour so some leeway is allowed
-                        let second = second
-                            .strip_prefix("FROM:")
-                            .unwrap_or_else(|| second)
-                            .to_string();
-                        let second = second
-                            .strip_prefix("<")
-                            .unwrap_or_else(|| &second)
-                            .strip_suffix(">")
-                            .unwrap_or_else(|| &second)
-                            .to_string();
-
-                        debug!("Stripped FROM header to {}", second);
-
-                        writer.write_all(&message_formatter("250 OK")).await?;
-                        writer.flush().await?;
-                        debug!("Responded to MAIL FROM");
-                    }
-                }
-                "RCPT" => {}
-                _ => {
-                    warn!("Unrecognised Command {}", first);
-                }
+                error!("Failed to split command (string: {:?}", cmd);
+                continue;
             }
         } else {
-            error!("Failed to split command (string: {:?}", cmd);
-            continue;
+            if let Some(first) = cmd.get(0) {
+                if first == "." {
+                    mail.sending_data = false;
+                    debug!("Finished Receving Data from connection");
+                    writer
+                        .write_all(&message_formatter("250 Message accepted"))
+                        .await?;
+                } else {
+                    debug!("Received Data: {line}");
+                    mail.data.push_str(line.replace("\r\n", "\n").as_str());
+                }
+            }
         }
     }
     Ok(())
